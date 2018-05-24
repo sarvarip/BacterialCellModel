ed6d72dd0c79fd265b2f1c0e51fd1abfd2218cb3
Peter Sarvari <sarvarip@gmail.com>
2018-05-24 21:43:47 +0100

    Add files via upload

---
+++ parsave.m
@@ -0,0 +1,3 @@
+function parsave(str, inst_gr_array, P_count_vec, transient_P, P_ss, time_elapsed, production_rate, tot_mass_end, tot_mass_ref, growth_rate, avg_inst_growth_rate, std_inst_growth_rate)
+save(str, 'inst_gr_array', 'P_count_vec', 'transient_P, P_ss', 'time_elapsed', 'production_rate', 'tot_mass_end', 'tot_mass_ref', 'growth_rate', 'avg_inst_growth_rate', 'std_inst_growth_rate')
+end
