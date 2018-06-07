558a21432eb91dcea70cce11c3ee9d1c52c6f98d
Peter Sarvari <sarvarip@gmail.com>
2018-06-07 14:08:25 +0100

    Add files via upload

--- Algar_sim_main.m
+++ Algar_sim_main.m
@@ -8,16 +8,22 @@ clear
 % 
 %edge case
 
-L = 19;
+tic;
+
+L = 200; %length
 M = 1;
 alfaplus = 1;
 alfaminus = 0;
-beta = ones(L+1,1); %everything shifted by one, since there is no idx zero in Matlab
-beta(10) = 0.2;
+beta = ones(L+1,1); %first index is free ribosomes
+slow_loc = 101;
+beta(slow_loc) = 0.1;
 
-tspan = [0 300];
-Y_ini = [10000, zeros(1,L+1)];
+tspan = [0 800];
+R0 = 1000;
+Y_ini = [R0, zeros(1,L+1)];
 [t, Y] = ode23(@(t,Y) simulation_Algar(t,Y,L,M,alfaplus,alfaminus,beta), tspan, Y_ini);
+
+toc;
 
 %Plot the density graph - reduce resolution by ~1000 so that matlab can
 %actually plot is w/o sinigificant computational issues
@@ -36,10 +42,12 @@ for j = 1:100
     y_red = Y(j*step,:);
     density_red(j) = sum(y_red(2:end))/(L+1);
 end
-plot(time_red, density_red, 'LineWidth', 2)
-title('Ribosome density on mRNA over time', 'FontSize', 20);
+plot(time_red, density_red, 'LineWidth', 3)
+%title('Ribosome density on mRNA over time', 'FontSize', 20);
 xlabel('Time', 'FontSize', 15);
 ylabel('Density', 'FontSize', 15);
+%legend({'Average of 10 simulations', 'RFM'}, 'FontSize', 15, 'Location','southeast');
+xlim([0 800])
 
 %Before slow codon
 time_red = zeros(100,1);
@@ -52,12 +60,14 @@ hold on
 for j = 1:100
     time_red(j) = t(j*step);
     y_red = Y(j*step,:);
-    density_red(j) = sum(y_red(2:11))/((L+1)/2);
+    density_red(j) = sum(y_red(2:slow_loc))/((L+1)/2);
 end
-plot(time_red, density_red, 'LineWidth', 2)
-title('Ribosome density on mRNA over time - Before slow codon', 'FontSize', 20);
+plot(time_red, density_red, 'LineWidth', 3)
+%title('Ribosome density on mRNA over time - Before slow codon', 'FontSize', 20);
 xlabel('Time', 'FontSize', 15);
 ylabel('Density', 'FontSize', 15);
+%legend({'Average of 10 simulations', 'RFM'}, 'FontSize', 15, 'Location','southeast');
+xlim([0 800])
 
 %After slow codon
 time_red = zeros(100,1);
@@ -70,10 +80,13 @@ hold on
 for j = 1:100
     time_red(j) = t(j*step);
     y_red = Y(j*step,:);
-    density_red(j) = sum(y_red(12:end))/((L+1)/2);
+    density_red(j) = sum(y_red(slow_loc+1:end))/((L+1)/2);
 end
-plot(time_red, density_red, 'LineWidth', 2)
-title('Ribosome density on mRNA over time - After slow codon', 'FontSize', 20);
+plot(time_red, density_red, 'LineWidth', 3)
+
+
+%title('Ribosome density on mRNA over time - After slow codon', 'FontSize', 20);
 xlabel('Time', 'FontSize', 15);
 ylabel('Density', 'FontSize', 15);
-    
+xlim([0 800])
+%legend({'Average of 10 simulations', 'RFM'}, 'FontSize', 15, 'Location','southeast');
--- RFM_STS_compare_1mRNA.m
+++ RFM_STS_compare_1mRNA.m
@@ -5,16 +5,18 @@
 close all
 clc, clear
 
+%% Multiple trajectories
+
 tic
 
-R0 = 10000; %vs 100; the one that suffers in production is the one with the least initiation rate
+R0 = 1000; %vs 100; the one that suffers in production is the one with the least initiation rate
 total_mRNA = 1;
 type_mRNA = 1;
 betas = cell(1,type_mRNA);
-betas{1} = [ones(1,9), 0.1, ones(1,10)];
-slow_loc = 10;
+betas{1} = [ones(1,99), 0.1, ones(1,100)];
+slow_loc = 100;
 type_idx_array = [1]; %DON'T forget to change this line!
-maxsteps = 700; 
+maxsteps = 15000; 
 no_sims = 10;
 coverage = zeros(no_sims,maxsteps+1);
 coverage_after_slowcod = zeros(no_sims,maxsteps+1);
@@ -65,12 +67,77 @@ for sim = 1:no_sims
     plot(time(sim,:),coverage_after_slowcod(sim,:)) %makes sense in terms of deterministic
 end
 
+%% Avg plots
 
-%Avg
-%plot(time(sim,:), mean(coverage_after_slowcod, 1), 'b', 'LineWidth', 3);
+% tic
+% 
+% R0 = 1000; %vs 100; the one that suffers in production is the one with the least initiation rate
+% total_mRNA = 1;
+% type_mRNA = 1;
+% betas = cell(1,type_mRNA);
+% betas{1} = [ones(1,99), 0.1, ones(1,100)];
+% slow_loc = 100;
+% type_idx_array = [1]; %DON'T forget to change this line!
+% no_sims = 10;
+% 
+% time_res=0:1:1000;
+% coverage_cell = cell(length(time_res),1);
+% coverage_after_slowcod_cell = cell(length(time_res),1);
+% coverage_before_slowcod_cell = cell(length(time_res),1);
+% 
+% for sim=1:no_sims
+%     time = 0;
+%     time_P_cell = cell(1,total_mRNA);
+%     P_count_vec = zeros(1,total_mRNA);
+%     state_array = ones(1,R0);
+%     location_array = zeros(1,R0);
+%     temp = [1]; %DON'T forget to change this line!
+%     [state_array, location_array, time, time_P_cell, P_count_vec, temp, transition_array] = Gillespie_STS_Prod_Rate_Multi_v6(state_array, location_array, betas, type_idx_array, time, time_P_cell, P_count_vec, temp);
+%     idx = find(time_res < time, 1, 'last');
+%     coverage_cell{idx}(end+1) = sum(state_array>1)/(length(betas{1})-1);
+%     coverage_after_slowcod_cell{idx}(end+1) = sum(state_array>slow_loc)/(length(betas{1})-slow_loc);
+%     coverage_before_slowcod_cell{idx}(end+1) = (sum(state_array>1) - sum(state_array>slow_loc))/(slow_loc-1);
+%     while time < time_res(end)
+%         [state_array, location_array, time, time_P_cell, P_count_vec, temp, transition_array] = Gillespie_STS_Prod_Rate_Multi_v6(state_array, location_array, betas, type_idx_array, time, time_P_cell, P_count_vec, temp, transition_array);
+%         idx = find(time_res < time, 1, 'last'); 
+%         coverage_cell{idx}(end+1) = sum(state_array>1)/(length(betas{1})-1);
+%         coverage_after_slowcod_cell{idx}(end+1) = sum(state_array>slow_loc)/(length(betas{1})-slow_loc);
+%         coverage_before_slowcod_cell{idx}(end+1) = (sum(state_array>1) - sum(state_array>slow_loc))/(slow_loc-1);
+%     end
+% end
+% 
+% toc
+% 
+% figure(1)
+% hold on
+% coverage_avg = zeros(1,length(coverage_cell));
+% for t = 1:length(coverage_cell)
+%     coverage_avg(t) = mean(coverage_cell{t});
+% end
+% scatter(time_res,coverage_avg, 'MarkerEdgeColor',[0 .5 .5],...
+%               'MarkerFaceColor',[0 .7 .7],...
+%               'LineWidth',1)
+% 
+% figure(2)
+% hold on
+% coverage_before_slowcod_avg = zeros(1,length(coverage_before_slowcod_cell));
+% for t = 1:length(coverage_before_slowcod_cell)
+%     coverage_before_slowcod_avg(t) = mean(coverage_before_slowcod_cell{t});
+% end
+% scatter(time_res,coverage_before_slowcod_avg, 'MarkerEdgeColor',[0 .5 .5],...
+%               'MarkerFaceColor',[0 .7 .7],...
+%               'LineWidth',1)
+% 
+% figure(3)
+% hold on
+% coverage_after_slowcod_avg = zeros(1,length(coverage_after_slowcod_cell));
+% for t = 1:length(coverage_after_slowcod_cell)
+%     coverage_after_slowcod_avg(t) = mean(coverage_after_slowcod_cell{t});
+% end
+% scatter(time_res,coverage_after_slowcod_avg, 'MarkerEdgeColor',[0 .5 .5],...
+%               'MarkerFaceColor',[0 .7 .7],...
+%               'LineWidth',1)
 
-%Avg
-%plot(time(sim,:), mean(coverage_before_slowcod, 1), 'b', 'LineWidth', 3);
 
 % time_elapsed = time(end)-time(1001);
 % transient_P=zeros(1,total_mRNA);
