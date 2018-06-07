645fd08efe2b916ab3d861e89020b360119f2e2c
Peter Sarvari <sarvarip@gmail.com>
2018-06-07 23:48:40 +0100

    Add files via upload

--- PieChartMovie.m
+++ PieChartMovie.m
@@ -1,14 +1,15 @@
 close all
-%load('FYP_04_06_par_ex_21')
+clear
+load('FYP_04_06_par_ex_15')
 idx = 1;
 aac_array = [7500, 300, 300, 300];
 for i = 1:10:4000
-    %figure
+    figure('units','normalized','outerposition',[0 0 1 1])
+    ax1 = axes('Position',[0.2 0.1 0.7 0.7],'Box','off');
+    ax2 = axes('Position',[0.1 0.7 0.2 0.2],'Box','off');
+    ax3 = axes('Position',[0.1 0.1 0.2 0.2],'Box','off');
+    ax = axes('Position',[0 0 1 1],'Box','off');
     if P_count_vec_array(i, 4) == 0
-        ax1 = axes('Position',[0.2 0.1 0.7 0.7],'Box','off');
-        ax2 = axes('Position',[0.1 0.7 0.2 0.2],'Box','off');
-        ax3 = axes('Position',[0.1 0.1 0.2 0.2],'Box','off');
-        ax = axes('Position',[0 0 1 1],'Box','off');
         axes(ax1)
         label = {'R-proteins: ', 'Enzyme: ', 'Housekeeping: '};
         p= pie(P_count_vec_array(i, :).*aac_array);
@@ -28,9 +29,6 @@ for i = 1:10:4000
         hText(2).String = combinedtxt(2);
         hText(3).String = combinedtxt(3);
     else
-        ax1 = axes('Position',[0.2 0.1 0.7 0.7],'Box','off');
-        ax2 = axes('Position',[0.1 0.7 0.2 0.2],'Box','off');
-        ax = axes('Position',[0 0 1 1],'Box','off');
         axes(ax1)
         label = {'R-proteins: ', 'Enzyme: ', 'Housekeeping: ', 'Heterologous: '};
         p= pie(P_count_vec_array(i, :).*aac_array);
@@ -46,18 +44,18 @@ for i = 1:10:4000
         hText(1).String = combinedtxt(1);
         hText(2).String = combinedtxt(2);
         hText(3).String = combinedtxt(3);
+        hText(4).String = combinedtxt(4);
         t = p(8);
         t.FontSize = 15;
         colormap([1 0 0;      %// red
           0 1 0;      %// green
           0 0 1;      %// blue
           .5 .5 .5])  %// grey
-        hText(4).String = combinedtxt(4);
     end
     title(['Proteome partition, time = ', num2str(time_ss(i))], 'FontSize', 15);
     axes(ax2);
     bar(energy_array(i), 'c')
-    ylim([0 15000])
+    %ylim([0 700000])
     title('Energy', 'FontSize', 15);
     ylabel('a.u.', 'FontSize', 15);
     set(gca,'xticklabel','')
@@ -73,17 +71,16 @@ for i = 1:10:4000
     ax.Units = 'normalized';
     disp(i)
     idx = idx+1;
-    hold off
-    %close
+    close
 end
 for i = 4100:100:10000
-    figure
+    figure('units','normalized','outerposition',[0 0 1 1])
     ax1 = axes('Position',[0.2 0.1 0.7 0.7],'Box','off');
     ax2 = axes('Position',[0.1 0.7 0.2 0.2],'Box','off');
     ax3 = axes('Position',[0.1 0.1 0.2 0.2],'Box','off');
     ax = axes('Position',[0 0 1 1],'Box','off');
     axes(ax1)
-    label = {'R-proteins', 'Enzyme', 'Housekeeping', 'Heterologous'};
+    label = {'R-proteins: ', 'Enzyme: ', 'Housekeeping: ', 'Heterologous: '};
     p= pie(P_count_vec_array(i, :).*aac_array);
     t = p(2);
     t.FontSize = 15;
@@ -103,10 +100,11 @@ for i = 4100:100:10000
     hText(1).String = combinedtxt(1);
     hText(2).String = combinedtxt(2);
     hText(3).String = combinedtxt(3);
+    hText(4).String = combinedtxt(4);
     title(['Proteome partition, time = ', num2str(time_ss(i))], 'FontSize', 15);
     axes(ax2);
     bar(energy_array(i), 'c')
-    ylim([0 15000])
+    %ylim([0 700000])
     title('Energy', 'FontSize', 15);
     ylabel('a.u.', 'FontSize', 15);
     set(gca,'xticklabel','')
@@ -118,15 +116,16 @@ for i = 4100:100:10000
     ylim([0 3])
     fig = gcf;
     fig.CurrentAxes = ax;
+    %FigH = figure('Position', get(0, 'Screensize'));
+    %M(idx) = getframe(FigH);
     M(idx) = getframe;
     ax.Units = 'normalized';
     disp(i)
     idx = idx+1;
-    hold off
     close
 end
-video = VideoWriter('04_06_ex21.avi', 'Uncompressed AVI');
-video.FrameRate = 15;  % Default 30
+video = VideoWriter('04_06_ex21_test.avi', 'Uncompressed AVI');
+video.FrameRate = 10;  % Default 30
 open(video)
 writeVideo(video, M);
 close(video);
