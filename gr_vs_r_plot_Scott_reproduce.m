e0fce816ffe251e15860ba951a7e1699f811871b
Peter Sarvari <sarvarip@gmail.com>
2018-12-19 15:55:55 -0800

    Add files via upload

--- gr_vs_r_plot_Scott_reproduce.m
+++ gr_vs_r_plot_Scott_reproduce.m
@@ -1,5 +1,9 @@
 figure; 
 r = [0.0092, 0.0134, 0.0147, 0.0170, 0.0238, 0.0247, 0.0255];
+%r obtained by calculating (total_transcript)*[7500 300 300]' / ((P_count_vec)[7500 300 300]')
+scaling_factor = 330*3/118.9; %Average weight of condon triplet in E.Coli divided by 
+%the average weight of an amino acid in E.Coli
+r = r*scaling_factor;
 gr = [0.2035, 0.8815, 1.7030, 2.7323, 4.3379, 4.7687, 4.81];
 one = [1 1 1 1 1 1 1];
 gr1 = [one; gr]';
